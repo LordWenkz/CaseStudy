071aa7d44545ce8eaf561ba4ded2da646bb8b46e
Serge-koy <vsergevalencia@yahoo.com>
2021-03-16 17:26:13 +0800

    This is Done.

---
+++ src/myMain.java
@@ -0,0 +1,18 @@
+
+public class myMain {
+
+    public static void main(String args[]){
+
+
+        Message itoNaTalaga = new Message();
+
+        itoNaTalaga.start();
+        itoNaTalaga.talk();
+
+
+        itoNaTalaga.Rafael();
+        itoNaTalaga.Serge();
+        itoNaTalaga.Angela();
+
+    }
+}
